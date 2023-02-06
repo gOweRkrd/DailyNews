ae9087dcadd81b8bef41f4619ddba1aca74b59e7
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-02-06 15:43:37 +0700

    Added network layer

---
+++ DailyNews/Services/Networking/Model/NewsModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+// MARK: - NewsModel
+struct APIResponse: Codable {
+    let articles: [Article]?
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+}
+
+struct Source: Codable {
+    let name : String
+}
+
---
+++ DailyNews/Services/Networking/NetworkService.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=d05f1b12009d457a877881f7cbde2e84")
+    }
+    
+    private init() {}
+    
+    // GET functions
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
+      
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles?.count)")
+                    completion(.success(result.articles!))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
